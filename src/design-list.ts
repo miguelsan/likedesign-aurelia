b111e493a61691fbb9fc67ea954d662e7a85ead2
Miguel San Miguel <development@miguelsanmiguel.com>
2016-09-09 11:37:54 +0200

    Autoinject HttpClient

--- src/design-list.ts
+++ src/design-list.ts
@@ -1,9 +1,13 @@
+import {autoinject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 
+@autoinject
 export class DesignList {
   routeConfig;
   designs = [];
   selectedId = 0;
+
+  constructor(private http: HttpClient) {}
 
   activate(params, routeConfig){
     this.routeConfig = routeConfig;
@@ -11,9 +15,8 @@ export class DesignList {
   }
 
   private fetchSearch(query, xmlParser) {
-    new HttpClient()
-    .configure(config => {
-      config
+    this.http
+    .configure(config => { config
       .withBaseUrl('https://api.spreadshirt.net/api/v1/shops/205909/designs')
     })
     .fetch('?query=' + query + '&mediaType=json')
