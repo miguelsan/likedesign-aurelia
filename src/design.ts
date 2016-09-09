71d2fe1c8c9b166f1333fc9ddb35c1bf2b002fe5
Miguel San Miguel <development@miguelsanmiguel.com>
2016-09-09 15:41:55 +0200

    Add like checkbox

--- src/design.ts
+++ src/design.ts
@@ -1,4 +1,8 @@
+import {bindable, bindingMode} from 'aurelia-framework';
+
 export class Design {
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) liked = undefined;
+
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
